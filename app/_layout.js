ee8ef0947ae9108eba48412cb0e10fff6cbe7e43
Mahbub192 <mahbubaligub192@gmail.com>
2024-03-29 11:37:17 +0600

    completed without convert pdf file

---
+++ app/_layout.js
@@ -0,0 +1,18 @@
+import { Stack } from 'expo-router'
+
+export default function _layout() {
+  return (
+    <Stack screenOptions={{
+        headerStyle:{
+            backgroundColor:"orange",
+        },
+        headerTintColor:"white",
+        headerTitleStyle:{
+            fontWeight:"bold"
+        }
+    }}>
+        <Stack.Screen name="index"/>
+        <Stack.Screen name="notes"/>
+    </Stack>
+  )
+}
